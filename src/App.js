3907e19c185a13fb142177abe1ecb512935c40af
Asma Selmi <Asma.selmi93@gmail.com>
2020-02-24 15:41:55 +0100

    git demo

--- src/App.js
+++ src/App.js
@@ -5,37 +5,156 @@ import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
-  state ={
-    persons: [
-      {name: 'Asma', age :'26'}, {name:'Nesrine', age:'22'}
-    ]
+  state = {
+    personsListA: [
+      { id: 'akjh', name: 'Asma', age: '26' },
+      { id: 'jgj', name: 'Nesrine', age: '22' },
+      { id: 'jkfjg', name: 'Leila', age: '52' },
+      { id: 'jhgg', name: 'Salah', age: '60' },
+      { id: 'jhfjg', name: 'Houda', age: '23' }
+    ],
+    personsListB: [],
+    showPersons: true,
+    style: {
+      color: 'blue'
+    }
   }
-   switchNameHandler = () => {
+
+  switchNameHandler = () => {
     console.log('buttom clicked')
     this.setState({
       persons: [
-        {name: 'Ons', age :'26'}, {name:'safa', age:'22'}
+        { name: 'Ons', age: '26' }, { name: 'safa', age: '22' }
       ]
     })
-// don't use this
+  }
+  changeList = (id) => {
+
+
+
+
+    const newPersons = [...this.state.personsListA]
+    const newPersonsB = [...this.state.personsListB]
+
+    const index = newPersons.findIndex(item => {
+      return item.id === id
+    })
+    const newP = newPersons[index]
+
+    newPersons.splice(index, 1)
+    this.setState({
+      personsListA: newPersons
+    })
+    newPersonsB.push(newP)
+    this.setState({
+      personsListB: newPersonsB
+
+    })
+
+
+
+  }
+
+
+
+
+  switchColor = () => {
+    console.log('buttom clicked')
+    this.setState({
+      style: {
+        color: 'red'
+      }
+    })
+
+
+
+  }
+
+  deleteNamehandler = (id) => {
+    const newPersons = [...this.state.persons]
+    const index = newPersons.findIndex(item => {
+      return item.id === id
+    })
+    newPersons.splice(index, 1)
+    this.setState({
+      persons: newPersons
+    })
+  }
+
+  changeNameHandler = (event, id) => {
+    console.log('change name handler')
+    const newPersons = [...this.state.persons]
+
+    const index = newPersons.findIndex(item => {
+      return item.id === id
+    })
+
+    newPersons[index].name = event.target.value
+    this.setState({
+      persons: newPersons
+    })
+
+    // don't use this
     // this.setState({
     //   persons: [
     //     {name: 'Ons', age :'26'}, {name:'safa', age:'22'}
     //   ]
     // })
   }
+  toggelPersonsHandler = () => {
+    console.log("yjfrk");
+
+    this.setState({ showPersons: !this.state.showPersons }
+    )
+  }
+
+
+  buildList = (persons) => {
+    return (
+      <div style={this.state.style}>
+        {persons.map(item => {
+          return <Person
+            name={item.name}
+            age={item.age}
+            changed={(event) => { this.changeNameHandler(event, item.id) }}
+            changeList={() => { this.changeList(item.id) }}
+            key={item.id} />
+        })}
+      </div>
+    )
+  }
+
+
   render() {
+    let personsListA = null
+    let personsListB = null
+    if (this.state.showPersons)
+      personsListA = this.buildList(this.state.personsListA)
+    personsListB = this.buildList(this.state.personsListB)
+
+
+
+
+
     return (
       // React.createElement('div', {
-        // className: 'App'
+      // className: 'App'
       // }, React.createElement('h1', null, 'Hello world'), React.createElement('Person', null, null))
-        <div className='App'>
-       <h1 >Hello react</h1>
-       <button onClick={this.switchNameHandler}>switch buttom</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-       
-        </div> 
+      <div className='App'>
+        <h1 >Hello react</h1>
+        <button onClick={this.toggelPersonsHandler}>Toggel Persons</button>
+        <button onClick={this.switchColor}>change color</button>
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: '50%' }}>
+            <h2>List A</h2>
+            {personsListA}
+          </div>
+          <div style={{ width: '50%' }}>
+            <h2>List B</h2>
+            {personsListB}
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,14 @@
 import React from 'react'
-const person =(props) =>{
-    return(
-<div>
-<p>My name is {props.name} and I'm {props.age}</p>
-</div>
+import classes from './Person.module.css'
+
+
+const person = (props) => {
+    return (
+        <div className={classes.Person}>
+
+            <p onClick={props.changeList}>My name is {props.name} and I'm {props.age}</p>
+            <input type='text' onChange={props.changed} />
+        </div>
     )
 }
 
